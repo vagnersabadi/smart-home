ddd9ddba21930db965056f3f40acb98ccbae3c82
vagnersabadi <vagnerbuzatta@hotmail.com>
2020-03-17 15:27:11 -0300

    style(): define theme

---
+++ lib/theme/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+//#0755de
+const ColorPrimary = const Color(0xff0755de);
+
+//#edf0f3
+const ColorSecondary = const Color(0xffedf0f3);
+
+//#b6b6b6
+const Grey1 = const Color(0xffb6b6b6);
+
+
+
+const ColorMuted = const Color.fromRGBO(255, 255, 255, 0.6);
+
+//Palette Colors
+//#0136c6
+//#5c6272
+//#0838aa
+//#afc3d6
+//#5e97d7
+
+
+
