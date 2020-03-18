8b83aa251a92f74cff17991bff5389928e426eee
vagnersabadi <vagnerbuzatta@hotmail.com>
2020-03-17 21:16:20 -0300

    feat(menu): list cards

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -9,12 +9,15 @@ const ColorSecondary = const Color(0xffedf0f3);
 //#b6b6b6
 const Grey1 = const Color(0xffb6b6b6);
 
+//#0136c6
+const Blue1 = const Color(0xff0136c6);
+
+
 
 
 const ColorMuted = const Color.fromRGBO(255, 255, 255, 0.6);
 
 //Palette Colors
-//#0136c6
 //#5c6272
 //#0838aa
 //#afc3d6
